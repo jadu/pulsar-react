7d31bdb893bf497c3202a483b6368132ef403cf9
Paul Stanton <paul@coffeepowered.co.uk>
2020-05-06 17:02:30 +0100

    Add more tests for InputType

--- src/components/form/TextInput/TextInput.test.js
+++ src/components/form/TextInput/TextInput.test.js
@@ -1,49 +1,93 @@
 'use strict';
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 
 
 import FormGroup from '../FormGroup';
 import TextInput from './TextInput';
 
-it('renders a form group with text input', () => {
-  const tree = shallow(
-    <FormGroup labelText="Basic">
-      <TextInput placeholder="foo" />
-    </FormGroup>
-  );
-  expect(tree).toMatchSnapshot();
-});
-
 it('renders the basic input', () => {
-  const tree = shallow(
+  const tree = render(
     <TextInput />
   );
   expect(tree).toMatchSnapshot();
 });
 
 it('renders the basic input with id', () => {
-  const tree = shallow(
+  const tree = render(
     <TextInput id="foo" />
   );
   expect(tree).toMatchSnapshot();
 });
 
+it('renders the basic input with idGuid', () => {
+  const tree = render(
+    <TextInput idGuid="foo" />
+  );
+  expect(tree).toMatchSnapshot();
+});
+
+it('will prefer id over idGuid', () => {
+  const tree = render(
+    <TextInput id="bar" idGuid="foo" />
+  );
+  expect(tree).toMatchSnapshot();
+});
+
 it('renders the basic input with required', () => {
-  const tree = shallow(
+  const tree = render(
     <TextInput required />
   );
   expect(tree).toMatchSnapshot();
 });
 
 it('renders the basic input with placeholder', () => {
-  const tree = shallow(
+  const tree = render(
     <TextInput placeholder="foo" />
   );
   expect(tree).toMatchSnapshot();
 });
 
+it('renders the basic input with aria-describedby', () => {
+  const tree = render(
+    <TextInput aria-describedby="foo" />
+  );
+  expect(tree).toMatchSnapshot();
+});
 
+it('renders as form group', () => {
+  const tree = shallow(
+    <FormGroup labelText="My label">
+      <TextInput />
+    </FormGroup>
+  );
+  expect(tree).toMatchSnapshot();
+});
 
+it('renders as form group, with helpText, using the aria-describedby attribute', () => {
+  const tree = shallow(
+    <FormGroup labelText="My label" helpText="foo">
+      <TextInput />
+    </FormGroup>
+  );
+  expect(tree).toMatchSnapshot();
+});
 
+it('renders as form group, with error, using the aria-describedby attribute', () => {
+  const tree = shallow(
+    <FormGroup labelText="My label" error="foo">
+      <TextInput />
+    </FormGroup>
+  );
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders as form group, with helpText and error, using two aria-describedby attributes', () => {
+  const tree = shallow(
+    <FormGroup labelText="My label" helpText="foo" error="bar">
+      <TextInput />
+    </FormGroup>
+  );
+  expect(tree).toMatchSnapshot();
+});
--- tasks/jest/setup.js
+++ tasks/jest/setup.js
@@ -3,9 +3,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// Mock the GUID generation to stop them being random
 jest.mock('shortid', () => {
   return {
-      generate: jest.fn(() => 1)
+    generate: jest.fn(() => 1)
   };
 });
 
