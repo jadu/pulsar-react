b5fd1b009c8bc808bce67bfa3875db1bbba3a177
Paul Stanton <paul@coffeepowered.co.uk>
2020-05-18 10:45:22 +0100

    Button helper

--- src/components/html/Button/Button.jsx
+++ src/components/html/Button/Button.jsx
@@ -4,7 +4,8 @@ import classnames from 'classnames';
 export default class Button extends React.Component {
 
   static defaultProps = {
-    className: 'btn'
+    className: 'btn',
+    tag: 'button'
   };
 
   render() {
@@ -13,25 +14,43 @@ export default class Button extends React.Component {
       children,
       className,
       danger,
+      href,
       id,
       idGuid,
       inverse,
       naked,
+      outline,
       primary,
+      small,
+      tag,
       success,
       warning,
       ...props
     } = this.props;
 
+    let ButtonTag = href ? 'a' : tag;
+
     let variantClasses = classnames(className, {
       'btn--danger': danger,
       'btn--inverse': inverse,
       'btn--naked': naked,
+      'btn--outline': outline,
       'btn--primary': primary,
       'btn--success': success,
+      'btn--small': small,
       'btn--warning': warning
     });
 
-    return <button className={variantClasses} {...props}>{children}</button>;
+    return (
+      <>
+        <ButtonTag 
+          className={variantClasses}
+          href={href ? href : null}
+          {...props}
+        >
+          {children}
+        </ButtonTag>
+      </>
+    );
   }
 }
