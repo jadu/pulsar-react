1ba579284e049701a9107db1379de74c0d3ed5de
jamesm <james.manley@jadu.netk>
2020-07-02 14:23:40 +0100

    Fixes to Icon

--- src/components/html/Icon/Icon.jsx
+++ src/components/html/Icon/Icon.jsx
@@ -2,28 +2,19 @@ import React from 'react';
 
 export default class Icon extends React.Component {
 
-  static defaultProps = {
-    aria-hidden: true
-  };
-
   render() {
     let {
-      aria-hiden,
       children,
       className,
       ...props
     } = this.props;
 
-    let loadingBlock = block ? (
-      <div className="loading-block">
-        {spinner}
-        {children &&
-          <span className="loading-block__message">{children}</span>
-        }
-      </div>
-    ) : spinner;
+    let ariaHidden = this.props['aria-hidden'];
+    if (ariaHidden === undefined) {
+      ariaHidden = true;
+    }
 
-    return <i className={className} aria-hidden={aria-hidden} {...props}>
+    return <i className={className} aria-hidden={ariaHidden} {...props}>
       {children}
     </i>;
   }
