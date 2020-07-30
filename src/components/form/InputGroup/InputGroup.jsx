657053b0340ee042e493fc80cefb6be1f8567ff7
Paul Stanton <paul@coffeepowered.co.uk>
2020-07-30 10:23:18 +0100

    Allow inputGroup to pass ID/ARIA attributes to child inputs

--- src/components/form/InputGroup/InputGroup.jsx
+++ src/components/form/InputGroup/InputGroup.jsx
@@ -4,14 +4,29 @@ export default class InputGroup extends React.Component {
 
   render() {
     let {
+      ariaDescribedby,
       children,
+      id,
       ...props
     } = this.props;
+
+    let childrenWithGuids = React.Children.map(
+      children,
+      (child, i) => {
+        if (child && child.props.className === 'form__control') {
+          return React.cloneElement(child, {
+            ariaDescribedby: ariaDescribedby,
+            id: id
+          });
+        }
+        return child;
+      }
+    );
 
     return (
       <div className="input-group has-btn-appended">
         {
-          children.map(function(child, i) {
+          childrenWithGuids.map(function(child, i) {
             if (child && child.props.tag === 'button') {
               return <span key={i} className="input-group-btn">{child}</span>
             }
