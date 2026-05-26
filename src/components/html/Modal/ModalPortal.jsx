26e41919965173dadb2673ce5eefdd4aa8f55f76
James Manley <james.manley@jadu.co.uk>
2026-05-26 15:21:20 +0100

    Fix ReactDOM confusion

--- src/components/html/Modal/ModalPortal.jsx
+++ src/components/html/Modal/ModalPortal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 export default class ModalPortal extends React.Component {
     constructor(props) {
